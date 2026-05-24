1d8691adfa3f2b58cb5a3a28dd8ab72f64bcd3d6
Bwonk <ozenmain@gmail.com>
2026-05-24 17:33:23 +0300

    fix: cms normal klasor olarak eklendi

---
+++ cms/src/api/editorial-quote/controllers/editorial-quote.ts
@@ -0,0 +1,7 @@
+/**
+ * editorial-quote controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::editorial-quote.editorial-quote');
---
+++ cms/src/api/editorial-quote/routes/editorial-quote.ts
@@ -0,0 +1,7 @@
+/**
+ * editorial-quote router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::editorial-quote.editorial-quote');
